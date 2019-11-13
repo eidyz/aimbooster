6b2922c792f5e646dc252fb273f52fdc9dd0053c
eidyz <eidyz819@gmail.com>
2019-11-13 21:40:43 +0100

    :sparkles: spawning multiple targets, handling hitboxes

---
+++ core/target/target.go
@@ -0,0 +1,96 @@
+package target
+
+import (
+	"image/color"
+
+	"github.com/eidyz/aimbooster/util"
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
+)
+
+//ResX ---
+const ResX = 800
+
+// ResY ---
+const ResY = 600
+
+// Target ---
+type Target struct {
+	X       float64
+	Y       float64
+	Size    float64
+	Speed   int
+	Color   color.Color
+	Grow    bool
+	Clicked bool
+}
+
+// Centerize --
+func Centerize(xIn, size float64) float64 {
+	x := (xIn - (size / 2) + 100)
+	return x
+}
+
+// New creates new Target
+func New(x, y, size float64, speed int) (target Target) {
+	target.X = x
+	target.Y = y
+	target.Size = size
+	target.Speed = speed
+	target.Color = color.RGBA{100, 200, 200, 0xff}
+	target.Grow = true
+	return
+}
+
+// NewRandom ---
+func NewRandom() (target Target) {
+	var size float64 = 1
+	x := float64(util.RandInt(0, ResX-150))
+	y := float64(util.RandInt(0, ResY-150))
+	return New(x, y, size, 500)
+}
+
+// Draw target
+func (target *Target) Draw(dst *ebiten.Image) {
+	ebitenutil.DrawRect(dst, Centerize(target.X, target.Size), Centerize(target.Y, target.Size), target.Size, target.Size, target.Color)
+}
+
+// CheckHit checks if target was clicked
+func (target *Target) CheckHit(dst *ebiten.Image) {
+	mouseX, mouseY := ebiten.CursorPosition()
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+		if float64(mouseX) >= Centerize(target.X, target.Size) &&
+			float64(mouseX) <= Centerize(target.X, target.Size)+target.Size &&
+			float64(mouseY) >= Centerize(target.Y, target.Size) &&
+			float64(mouseY) <= Centerize(target.Y, target.Size)+target.Size {
+			// DEBUG HITBOXES
+			/* ebitenutil.DrawRect(dst, Centerize(target.X, target.Size), Centerize(target.Y, target.Size), 2, 2, color.RGBA{255, 0, 0, 255})
+			ebitenutil.DrawRect(dst, Centerize(target.X, target.Size)+target.Size, Centerize(target.Y, target.Size), 2, 2, color.RGBA{255, 0, 0, 255})
+			ebitenutil.DrawRect(dst, Centerize(target.X, target.Size)+target.Size, Centerize(target.Y, target.Size)+target.Size, 2, 2, color.RGBA{255, 0, 0, 255})
+			ebitenutil.DrawRect(dst, Centerize(target.X, target.Size), Centerize(target.Y, target.Size)+target.Size, 2, 2, color.RGBA{255, 0, 0, 255}) */
+			target.Clicked = true
+		}
+	}
+}
+
+// Pulse target
+func (target *Target) Pulse() {
+	if target.Grow {
+		if target.Size >= 100 {
+			target.Grow = false
+		}
+		target.Size++
+	} else {
+		if target.Size <= 0 {
+			target.Grow = true
+		}
+		target.Size--
+	}
+}
+
+// Init new random Target
+func Init() (target Target) {
+	target = NewRandom()
+	target.Pulse()
+	return
+}
--- util/util.go
+++ util/util.go
@@ -1,13 +1,14 @@
 package util
 
 import (
+	"math/rand"
 	"time"
 )
 
 //SetInterval Sets async interval
 func SetInterval(someFunc func(), milliseconds int, async bool) chan bool {
 
-	// How often to fire the passed in function 
+	// How often to fire the passed in function
 	// in milliseconds
 	interval := time.Duration(milliseconds) * time.Millisecond
 
@@ -19,27 +20,37 @@ func SetInterval(someFunc func(), milliseconds int, async bool) chan bool {
 	// Put the selection in a go routine
 	// so that the for loop is none blocking
 	go func() {
-			for {
+		for {
 
-					select {
-					case <-ticker.C:
-							if async {
-									// This won't block
-									go someFunc()
-							} else {
-									// This will block
-									someFunc()
-							}
-					case <-clear:
-							ticker.Stop()
-							return
-					}
-
+			select {
+			case <-ticker.C:
+				if async {
+					// This won't block
+					go someFunc()
+				} else {
+					// This will block
+					someFunc()
+				}
+			case <-clear:
+				ticker.Stop()
+				return
 			}
+
+		}
 	}()
 
-	// We return the channel so we can pass in 
+	// We return the channel so we can pass in
 	// a value to it to clear the interval
 	return clear
 }
 
+// RandInt generates random Float64 in range
+func RandInt(min int, max int) int {
+	n := 1
+	arr := make([]int, n)
+	var r int
+	for r = 0; r <= n-1; r++ {
+		arr[r] = rand.Intn(max) + min
+	}
+	return arr[0]
+}
